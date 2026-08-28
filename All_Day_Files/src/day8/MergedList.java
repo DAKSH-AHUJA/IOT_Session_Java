a94b39d6b27bcfdc92d36a299870e4a35c82157d
Daksh <dakshahuja31106@gmail.com>
2026-08-28 10:56:38 +0530

    added some files from session 1

--- All_Day_Files/src/day8/MergedList.java
+++ All_Day_Files/src/day8/MergedList.java
@@ -1,63 +1,29 @@
 package day8;
-
 public class MergedList {
-	static class Node {
-		int data;
-		Node next;
-
-		Node(int data) {
-			this.data = data;
-		}
-	}
-	public static void main(String[] args) {
-
-		Node first1 = new Node(10);
-		first1.next = new Node(30);
-		first1.next.next = new Node(50);
-
-		Node first2 = new Node(20);
-		first2.next = new Node(40);
-		first2.next.next = new Node(60);
-
-		Node first = null;
-		Node last = null;
-
-		while (first1 != null && first2 != null) {
-
-			Node newNode;
-
-			if (first1.data < first2.data) {
-				newNode = first1;
-				first1 = first1.next;
-			} else {
-				newNode = first2;
-				first2 = first2.next;
-			}
-
-			if (first == null) {
-				first = newNode;
-				last = newNode;
-			} else {
-				last.next = newNode;
-				last = newNode;
-			}
-		}
-
-		if (first1 != null) {
-			last.next = first1;
-		}
-
-		if (first2 != null) {
-			last.next = first2;
-		}
-
-		Node current = first;
-
-		while (current != null) {
-			System.out.print(current.data + " -> ");
-			current = current.next;
-		}
-
-		System.out.println("Null");
-	}
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        a.next = new Node(20);
+        a.next.next = new Node(30);
+        Node b = new Node(40);
+        b.next = new Node(50);
+        b.next.next = new Node(60);
+        Node current = a;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = b;
+        System.out.println("Merged Linked List:");
+        current = a;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
 }
