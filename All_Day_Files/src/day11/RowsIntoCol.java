069c664ee23474c4e706746106d6d9289b46ed54
Daksh <dakshahuja31106@gmail.com>
2026-09-02 16:11:27 +0530

    Session 3 files 02 sep

---
+++ All_Day_Files/src/day11/RowsIntoCol.java
@@ -0,0 +1,28 @@
+package day11;
+public class RowsIntoCol {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] transposed = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < transposed.length; i++) {
+            for (int j = 0; j < transposed[i].length; j++) {
+                System.out.print(transposed[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
